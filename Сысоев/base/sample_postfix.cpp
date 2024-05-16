82aeb3420bfa2dd82a8a6dc96ce891e6c0b3e40b
Kirill Yurlov <146000587+yurlovk@users.noreply.github.com>
2024-05-17 02:11:35 +0300

    Update sample_postfix.cpp

--- Сысоев/base/sample_postfix.cpp
+++ Сысоев/base/sample_postfix.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 int main()
 {
-  string expression;
-  TPostfix postfix;
-  double res;
+    string expression;
+    double res;
 
-  setlocale(LC_ALL, "Russian");
-  //cout << "Введите арифметическое выражение: ";
-  //cin >> expression;
-  //cout << expression << endl;
-  cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
-  postfix.ToPostfix();
-  cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
-  res = postfix.Calculate();
-  cout << res << endl;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите арифметическое выражение: ";
+    cin >> expression;
+    cout << expression << endl;
+  
+    TPostfix postfix(expression);
+  
+    cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
+    //postfix.ToPostfix();
+    cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
+    res = postfix.Calculate(cin, cout);
+    cout << "Результат: " << res << endl;
 
-  return 0;
+    return 0;
 }
