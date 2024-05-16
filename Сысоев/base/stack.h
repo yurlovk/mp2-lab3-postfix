b966b6f4c0e57dd1c5e46446d323118cd9ba4f13
Kirill Yurlov <146000587+yurlovk@users.noreply.github.com>
2024-05-17 01:49:15 +0300

    Update stack.h

--- Сысоев/base/stack.h
+++ Сысоев/base/stack.h
@@ -1,27 +1,63 @@
 ﻿#ifndef __STACK_H__
 #define __STACK_H__
 
-const int MaxStackSize = 100;
+#include <vector>
+#include <cstddef>
+#include <stdexcept>
 
-template <class T>
-class TStack
+using namespace std;
+template <typename T>
+class Stack
 {
-  T *pMem;
-  int size;
-  int top;
-public:
-  TStack(int _size)
-  {
-    size = _size;
-    top = -1;
-    if ((size < 1) || (size > MaxStackSize))
-      throw size;
-    pMem = new T[size];
-  }
-  ~TStack()
-  {
-    delete [] pMem;
-  }
-};
+    vector<T> vec;
+    size_t sz;
+public:Stack()
+    {
+        sz = 0;
+    }
+    Stack(size_t size) : Stack()
+    {
+        vec.reserve(size);
+    }
+    Stack(size_t size, const T* arr) : Stack(size)
+    {
+        for (int i = 0; i < size; i++)
+            vec.push_back(arr[i]);
+        sz = size;
+    }
 
+    size_t size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return sz == 0;
+    }
+
+    bool operator==(const Stack& st)
+    {
+        if (this == &st)
+            return true;
+        return st.vec == vec;
+    }
+    void push(const T& elem)
+    {
+        vec.push_back(elem);
+        sz++;
+    }
+    T top()
+    {
+        if (empty())
+            throw std::out_of_range("Empty stack");
+        return vec[sz - 1];
+    }
+    void pop()
+    {
+        if (empty())
+            throw std::out_of_range("Empty stack");
+        vec.pop_back();
+        sz--;
+    }
+};
 #endif
